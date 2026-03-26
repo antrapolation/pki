d4541ca987c2894f111c5d6bf0e9adae34a42c1e
amir-climy <amirrudin.yahaya@gmail.com>
2026-03-27 00:40:41 +0800

    feat(pki_tenancy): create mix project skeleton

---
+++ src/pki_tenancy/lib/pki_tenancy.ex
@@ -0,0 +1,3 @@
+defmodule PkiTenancy do
+  @moduledoc "Shared multi-tenancy library for the PKI system."
+end
---
+++ src/pki_tenancy/mix.exs
@@ -0,0 +1,40 @@
+defmodule PkiTenancy.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :pki_tenancy,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      aliases: aliases()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.11"},
+      {:postgrex, "~> 0.18"},
+      {:jason, "~> 1.4"},
+      {:uniq, "~> 0.6"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+    ]
+  end
+end
